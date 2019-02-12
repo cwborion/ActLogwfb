6fee73aab92442a98a3af033aa228c5cf3777834
Caleb Brenner <calebwill91@gmail.com>
2019-02-12 15:04:20 -0600

    no list item message renders now for all collections, but still disappears if navlink in navbar  for collection being visited is clicked again before navigating elsewhere

--- src/components/expenses/ExpenseList.js
+++ src/components/expenses/ExpenseList.js
@@ -5,14 +5,25 @@ import { Link } from 'react-router-dom'
 const ExpenseList = ({ expenses, auth }) => {
   return (
     <div className="section">
-
       {expenses && expenses.map(expense => {
+        const filtered = expenses.filter((expense, index) => {
+          return expenses[index].userId === auth.uid;
+        });
         if (expense.userId === auth.uid) {
           return (
             <Link to={'/expense/' + expense.id} key={expense.id}>
               <ExpenseSummary expense={expense} id={expense.id} />
             </Link>
           )
+        } else if (filtered.length === 0) {
+          expenses.length = 0;
+          // For some reason when the 'Expenses' navlink is clicked in the navbar when the 'no expenses' 
+          // message is already rendering, it disappears. Try to fix this.
+          return filtered.length === 0 ? 
+            <p key={1} className='white-text'>
+            Looks like you don't have any expenses. Click the 'Add Expense' button to start!
+            </p> :
+            null;
         } else {
           return null;
         }
--- src/components/income/IncomeList.js
+++ src/components/income/IncomeList.js
@@ -2,23 +2,25 @@ import React from 'react'
 import IncomeSummary from './IncomeSummary'
 import { Link } from 'react-router-dom'
 
-const IncomeList = ({ incomes, auth }) => {
+const IncomeList = ({ income, auth }) => {
   return (
     <div className="section">
-      {incomes && incomes.map((income, index ) => {
-        console.log('incomes is', incomes);
-        const filtered = incomes.filter((income, index) => {
-          return incomes[index].userId === auth.uid;
+      {income && income.map((incomeObj, index ) => {
+        console.log('income is', income);
+        const filtered = income.filter((value, index) => {
+          return income[index].userId === auth.uid;
         });
         console.log('filtered income array is', filtered)
-        if (income.userId === auth.uid) {
+        if (incomeObj.userId === auth.uid) {
+          // used slightly different naming convention 'incomes' being plural to
+          // differentiate between the array and the individual income as 'incomeObj'
           return (
-            <Link to={'/income/' + income.id} key={income.id}>
-              <IncomeSummary income={income} id={income.id} />
+            <Link to={'/income/' + incomeObj.id} key={incomeObj.id}>
+              <IncomeSummary income={incomeObj} id={incomeObj.id} />
             </Link>
           )
         } else if (filtered.length === 0) {
-          incomes.length = 0;
+          income.length = 0;
           // For some reason when the 'Todos' navlink is clicked in the navbar when the 'no todos' 
           // message is already rendering, it disappears. Try to fix this.
           return filtered.length === 0 ? 
