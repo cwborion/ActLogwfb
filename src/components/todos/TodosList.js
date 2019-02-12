d36065506073df0b94444887b3e9a823f6162a9e
Caleb Brenner <calebwill91@gmail.com>
2019-02-11 21:02:26 -0600

    trying to make message render when not todos visible, experimenting with splice method

--- src/components/todos/TodosList.js
+++ src/components/todos/TodosList.js
@@ -5,16 +5,25 @@ import { Link } from 'react-router-dom'
 const TodosList = ({ todos, auth }) => {
   return (
     <div className="section">
-      {todos && todos.map(todo => {
-        // console.log('todos is', todo.userId);
+      {todos && todos.map((todo, index) => {
+        console.log('todos is', index);
         // console.log('auth is', auth.uid);
-        if (todo.userId === auth.uid) {
+        const item = todos[index].userId === auth.uid;
+        if (todos[index].userId === auth.uid) {
+          // todos.splice(todos.indexOf(item), 1);
           return (
             <Link to={'/todo/' + todo.id} key={todo.id}>
               <TodoSummary todo={todo} id={todo.id} />
             </Link>
           )
-        } else {
+        } 
+        else if (todos[index].userId === !auth.uid) {
+          todos.splice(todos.indexOf(item), 1)
+          return (
+            <p className='white-text'>Looks like you have no todos</p>
+          )
+        } 
+        else {
           return null;
         }
       })}
